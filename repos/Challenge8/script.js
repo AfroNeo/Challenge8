d3348ffd4913a54432054822745f38593ca5a355
Matthew <mbrooks9622@gmail.com>
2023-01-27 15:04:37 +0000

    api key implemented

---
+++ repos/Challenge8/script.js
@@ -0,0 +1,23 @@
+
+//put fetch inside of an event listener
+//My weather API key 840e2eedb61362e6a6b45816372f15b9
+// fetch("api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}")
+
+fetch("api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=840e2eedb61362e6a6b45816372f15b9")
+    .then(response => response.json())
+    
+    
+
+
+
+
+//on click fire off....grab the value from the button its self, you can pull off the string from the button and use delegation
+//event.target...will grab the event of the city.
+
+//focus on the functionaliy
+//get the buttons working with local storage and getting the data back.
+
+
+
+
+
